b3407cb29251258718964347101c4a7aaed870e4
Lương Văn Tú <tuluong@mashallah.app>
2024-11-05 08:50:39 +0700

    set options

--- translation.go
+++ translation.go
@@ -1,12 +1,22 @@
 package i18n
 
-import "golang.org/x/text/language"
+import (
+	"reflect"
+
+	"golang.org/x/text/language"
+)
 
 type I18nProvider interface {
 	T(key string, l language.Tag) string
 }
 
-type I18n struct{}
+type I18n struct {
+	Dir              string
+	FileType         I18nTranslationFileType
+	Fallback         language.Tag
+	UseNamespace     bool
+	DefaultNamespace string
+}
 
 func (i *I18n) T(key string, lang language.Tag) string {
 	// Default return key
@@ -20,6 +30,23 @@ func NewI18n(opts ...*I18nConfigOptionsBuilder) (I18nProvider, error) {
 	args, err := NewOptions(opts...)
 	if err != nil {
 		return nil, err
+	}
+
+	if args.Dir != "" {
+		i.Dir = args.Dir
+	}
+
+	i.FileType = FileTypeJSON
+	if !reflect.ValueOf(args.FileType).IsNil() {
+		i.FileType = args.FileType
+	}
+
+	if args.UseNamespace {
+		i.UseNamespace = true
+	}
+
+	if args.DefaultNamespace != "" {
+		i.DefaultNamespace = args.DefaultNamespace
 	}
 
 	return i, nil
